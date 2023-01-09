a3a893952921f6f7f5df2363620c068d46684756
Aamir Maniar <aamironline@gmail.com>
2023-01-09 10:44:46 +0530

    Generics removed to provide TinyGo compatibility

--- format.go
+++ format.go
@@ -8,22 +8,24 @@ import (
 // Format converts a simple format string to a time.Time format string. It
 // accepts a simple format string such as "yyyy-mm-dd" and returns a converted
 // format string such as "2006-01-02".
-func Format[T DateTime](dt T, format string) (string, error) {
+func Format(dt any, format string) (string, error) {
 	convertedFormat := ConvertFormat(format)
-	var dtAny interface{} = dt
-	dtType := fmt.Sprintf("%T", dtAny)
 
-	if dtType == "time.Time" {
-		return dtAny.(time.Time).Format(convertedFormat), nil
-	} else if dtType == "string" {
+	switch dtType := dt.(type) {
+	case time.Time:
+		return dtType.Format(convertedFormat), nil
+	case string:
 		// Convert string to time.Time using RFC3339 format
-		t, err := time.Parse(time.RFC3339, dtAny.(string))
+		t, err := time.Parse(time.RFC3339, dtType)
 		if err != nil {
 			return "", err
 		}
 		return t.Format(convertedFormat), nil
-	} else if dtType == "int64" {
-		return time.Unix(dtAny.(int64), 0).Format(convertedFormat), nil
+	case int64:
+		return time.Unix(dtType, 0).Format(convertedFormat), nil
+	case uint64:
+		return time.Unix(int64(dtType), 0).Format(convertedFormat), nil
 	}
-	return time.Unix(int64(dtAny.(uint64)), 0).Format(convertedFormat), nil
+
+	return "", fmt.Errorf("Invalid type %T, expecting time.Time, string, int64 or uint64", dt)
 }
