e080728c74d8b41fde8439b0f42151b1c0392ecc
Kabir Maniar <kabir.maniar@maniartech.com>
2023-01-17 21:05:20 +0530

    Updated the code structure and optimized it.

--- time_ago.go
+++ time_ago.go
@@ -23,34 +23,39 @@ func TimeAgo(date time.Time) string {
 	timeVal := 0
 	future := false
 	timeSince := time.Since(date)
+	seconds := int(timeSince.Seconds())
 	// If timeSince is negative, then the date is in the future
 	if timeSince < 0 {
 		timeSince = timeSince * -1
 		future = true
 	}
-
-	// Checking the timeScale from the smallest level {Just Now, Few Minutes ago, ... }
-	if timeSince.Hours() < 1 {
-		if timeSince.Minutes() < 1 {
-			if future {
-				return "In a few seconds"
-			}
-			return "Just now"
-		} else {
-			if future {
-				return "In a few minutes"
-			}
-			return "Few minutes ago"
+	// Checking through all the time scales from seconds to years
+	if future {
+		if seconds < 10 {
+			return "In a few seconds"
+		} else if seconds < 60 {
+			return "In a minute"
+		} else if seconds < 3600 {
+			return "In a few minutes"
 		}
+	}
+
+	//Past
+	if seconds < 10 {
+		return "Just now"
+	} else if seconds < 60 {
+		return "A Minute ago"
+	} else if seconds < 3600 {
+		return "A few minutes ago"
 	}
 
 	// Going through all the time scales and finding the closest one
 	hours := timeSince.Hours()
 	for i, val := range hoursList[:4] {
-
 		if hours >= val && hours < hoursList[i+1] {
 			timeVal = int(math.Round(hours / val))
 			timeScaleVal = timeScale[i]
+			break
 		}
 	}
 	if timeScaleVal == "" {
@@ -58,39 +63,43 @@ func TimeAgo(date time.Time) string {
 		timeScaleVal = timeScale[4]
 	}
 
-	// If the time scale is in days and the date is in the future,
-	// then we need to print "Tomorrow" and "Yesterday" instead of "In a day" and "Last day"
+	// Returning the time difference if the date is in the future
+	if future {
+		// If the date is after tomorrow midnight and before day after tomorrow midnight then print "Tomorrow"
+		if timeScaleVal == "days" || timeScaleVal == "hours" {
+			now := time.Now().UTC()
+			nowYear, nowMonth, nowDay := now.Date()
+
+			dayAfterTomorrowMidnight := Date(nowYear, int(nowMonth), nowDay+2)
+			tomorrowMidnight := Date(nowYear, int(nowMonth), nowDay+1)
+
+			//If the date is after tomorrow midnight and before day after tomorrow midnight then print "Tomorrow"
+			if date.After(tomorrowMidnight) && date.Before(dayAfterTomorrowMidnight) {
+				return "Tomorrow"
+			}
+		}
+
+		// If the time difference is 1, then we need to print singular form of the time scale
+		if timeVal == 1 {
+			return fmt.Sprintf("In a %s", timeScaleVal[:len(timeScaleVal)-1])
+		}
+		return fmt.Sprintf("In %d %s", timeVal, timeScaleVal)
+	}
+	// Past
+	// If the date is before yesterday midnight and after day before yesterday midnight then print "Yesterday"
 	if timeScaleVal == "days" || timeScaleVal == "hours" {
 
 		now := time.Now().UTC()
 		nowYear, nowMonth, nowDay := now.Date()
 
 		// Calculating the midnight of the day after tomorrow, tomorrow, yesterday and day before yesterday
-		dayAfterTomorrowMidnight := Date(nowYear, int(nowMonth), nowDay+2)
-		tomorrowMidnight := Date(nowYear, int(nowMonth), nowDay+1)
 		yesterdayMidnight := Date(nowYear, int(nowMonth), nowDay)
 		dayBeforeYesterdayMidnight := Date(nowYear, int(nowMonth), nowDay-1)
 
-		//If the date is after tomorrow midnight and before day after tomorrow midnight then print "Tomorrow"
-		if date.After(tomorrowMidnight) && date.Before(dayAfterTomorrowMidnight) {
-
-			return "Tomorrow"
-		}
 		if date.After(dayBeforeYesterdayMidnight) && date.Before(yesterdayMidnight) {
 
 			return "Yesterday"
 		}
-	}
-
-	// Printing the time difference
-	// If the time difference is 1, then we need to print singular form of the time scale
-
-	// Returning the time difference if the date is in the future
-	if future {
-		if timeVal == 1 {
-			return fmt.Sprintf("In a %s", timeScaleVal[:len(timeScaleVal)-1])
-		}
-		return fmt.Sprintf("In %d %s", timeVal, timeScaleVal)
 	}
 
 	// Returning the time difference if the date is in the future
