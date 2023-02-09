fce9779b4d2539c8f3448147b0f8b360f275ae7e
Kabir Maniar <kabir.maniar@maniartech.com>
2023-02-09 15:53:26 +0530

    Updated date range tests

--- daterange_test.go
+++ daterange_test.go
@@ -7,290 +7,120 @@ import (
 	"github.com/maniartech/temporal"
 )
 
-// Covers all test cases for the function RelativeRange in daterange.go
-func TestRelativeRange(t *testing.T) {
+// Tests for DateRange.For
+func TestDateRangeFor(t *testing.T) {
+	d := temporal.DateRange()
 
-	now := time.Now().UTC()
-	todayMidnight := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
-
-	t.Run("today", func(t *testing.T) {
-		// Test case for today
-		d1, d2, err := temporal.RelativeRange("today")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 1))
-	})
-
-	t.Run("yesterday", func(t *testing.T) {
-		// Test case for yesterday
-		d1, d2, err := temporal.RelativeRange("yesterday")
-		if err != nil {
-			t.Error(err)
-		}
-		yesterday := todayMidnight.AddDate(0, 0, -1)
-		isEqual(t, *d1, yesterday)
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("tomorrow", func(t *testing.T) {
-		// Test case for tomorrow
-		d1, d2, err := temporal.RelativeRange("tomorrow")
-		if err != nil {
-			t.Error(err)
-		}
-		tomorrow := todayMidnight.AddDate(0, 0, 1)
-		isEqual(t, *d1, tomorrow)
-		isEqual(t, *d2, tomorrow.AddDate(0, 0, 1))
-	})
-
-	t.Run("last-<n>days", func(t *testing.T) {
-		// Test case for last-<n>days
-		d1, d2, err := temporal.RelativeRange("last-5days")
-		if err != nil {
-			t.Error(err)
-		}
-		last5days := todayMidnight.AddDate(0, 0, -5)
-		isEqual(t, *d1, last5days)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 1))
-	})
-
-	t.Run("next-<n>days", func(t *testing.T) {
-		// Test case for next-<n>days
-		d1, d2, err := temporal.RelativeRange("next-5days")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 6))
-	})
-
-	t.Run("thisweek", func(t *testing.T) {
-		// Test case for thisweek
-		d1, d2, err := temporal.RelativeRange("thisweek")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 7))
-	})
-
-	t.Run("lastweek", func(t *testing.T) {
-		// Test case for lastweek
-		d1, d2, err := temporal.RelativeRange("lastweek")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, 0, -7))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("nextweek", func(t *testing.T) {
-		// Test case for nextweek
-		d1, d2, err := temporal.RelativeRange("nextweek")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, 0, 7))
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 14))
-	})
-
-	t.Run("last-<n>weeks", func(t *testing.T) {
-		// Test case for last-<n>weeks
-		d1, d2, err := temporal.RelativeRange("last-2weeks")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, 0, -14))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("next-<n>weeks", func(t *testing.T) {
-		// Test case for next-<n>weeks
-		d1, d2, err := temporal.RelativeRange("next-2weeks")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 0, 14))
-	})
-
-	t.Run("thismonth", func(t *testing.T) {
-		// Test case for thismonth
-		d1, d2, err := temporal.RelativeRange("thismonth")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 1, 0))
-	})
-
-	t.Run("lastmonth", func(t *testing.T) {
-		// Test case for lastmonth
-		d1, d2, err := temporal.RelativeRange("lastmonth")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, -1, 0))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("nextmonth", func(t *testing.T) {
-		// Test case for nextmonth
-		d1, d2, err := temporal.RelativeRange("nextmonth")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, 1, 0))
-		isEqual(t, *d2, todayMidnight.AddDate(0, 2, 0))
-	})
-
-	t.Run("last-<n>months", func(t *testing.T) {
-		// Test case for last-<n>months
-		d1, d2, err := temporal.RelativeRange("last-2months")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(0, -2, 0))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("next-<n>months", func(t *testing.T) {
-		// Test case for next-<n>months
-		d1, d2, err := temporal.RelativeRange("next-2months")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(0, 2, 0))
-	})
-
-	t.Run("thisyear", func(t *testing.T) {
-		// Test case for thisyear
-		d1, d2, err := temporal.RelativeRange("thisyear")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(1, 0, 0))
-	})
-
-	t.Run("lastyear", func(t *testing.T) {
-		// Test case for lastyear
-		d1, d2, err := temporal.RelativeRange("lastyear")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(-1, 0, 0))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("nextyear", func(t *testing.T) {
-		// Test case for nextyear
-		d1, d2, err := temporal.RelativeRange("nextyear")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(1, 0, 0))
-		isEqual(t, *d2, todayMidnight.AddDate(2, 0, 0))
-	})
-
-	t.Run("last-<n>years", func(t *testing.T) {
-		// Test case for last-<n>years
-		d1, d2, err := temporal.RelativeRange("last-2years")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight.AddDate(-2, 0, 0))
-		isEqual(t, *d2, todayMidnight)
-	})
-
-	t.Run("next-<n>years", func(t *testing.T) {
-		// Test case for next-<n>years
-		d1, d2, err := temporal.RelativeRange("next-2years")
-		if err != nil {
-			t.Error(err)
-		}
-		isEqual(t, *d1, todayMidnight)
-		isEqual(t, *d2, todayMidnight.AddDate(2, 0, 0))
-	})
-
-	// Test case for an empty string
-	_, _, err := temporal.RelativeRange("")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
+	if time.Until(d.For) > time.Second*1 { // time.Until returns the duration until t (time.Now() in
+		t.Error("For date is not now")
 	}
 
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("next-xdays")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("next-xweeks")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("next-xmonths")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("next-xyears")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("last-xdays")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("last-xweeks")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("last-xmonths")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case that leads to error by Atoi function
-	_, _, err = temporal.RelativeRange("last-xyears")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-	// Test case when no match is found in the branch
-	_, _, err = temporal.RelativeRange("next-xyzpqr")
-	if err == nil {
-		t.Errorf("Expected %v, got, nil", err)
-	}
-
-}
-
-// isEqual compares two temporal values and returns true if they are equal
-func isEqual(t *testing.T, d1 time.Time, d2 time.Time) {
-	if d1.IsZero() || d2.IsZero() {
-		t.Error("Expected non-zero time")
-	}
-	if d1 != d2 {
-		t.Error("Expected equal time", d1, d2)
+	baseTime := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+	d = temporal.DateRange(baseTime)
+	if d.For != baseTime {
+		t.Error("For date is not base time")
 	}
 }
 
-func BenchmarkRelativeRange(b *testing.B) {
-	// Benchmarking for RelativeRange function
-	for i := 0; i < b.N; i++ {
-		temporal.RelativeRange("next-10years")
+// Tests for DateRange.Today
+func TestDateRangeToday(t *testing.T) {
+	d := temporal.DateRange().Today()
+
+	if d.From != temporal.DayStart() {
+		t.Error("From date is not today's start date")
+	}
+	if d.To != temporal.DayEnd() {
+		t.Error("To date is not today's end date")
+	}
+}
+
+// Tests for DateRange.Yesterday
+func TestDateRangeYesterday(t *testing.T) {
+	d := temporal.DateRange().Yesterday()
+
+	if d.From != temporal.Yesterday() {
+		t.Error("From date is not yesterday's date")
+	}
+	if d.To != temporal.DayEnd() {
+		t.Error("To date is not today's end date")
+	}
+}
+
+// Tests for DateRange.Tomorrow
+func TestDateRangeTomorrow(t *testing.T) {
+	d := temporal.DateRange().Tomorrow()
+
+	if d.From != temporal.DayStart() {
+		t.Error("From date is not today's start date")
+	}
+	if d.To != temporal.Tomorrow() {
+		t.Error("To date is not tomorrow's date")
+	}
+}
+
+// Tests for DateRange.Days
+func TestDateRangeDays(t *testing.T) {
+	d := temporal.DateRange().Days(-2)
+
+	if d.From != temporal.DayStart().AddDate(0, 0, -1) { // -1 because the base date is included
+		t.Error("From date is not 2 days ago")
+	}
+	if d.To != temporal.DayEnd() {
+		t.Error("To date is not today's end date")
+	}
+
+	d = temporal.DateRange().Days(2)
+
+	if d.From != temporal.DayStart() {
+		t.Error("From date is not 2 days from now")
+	}
+	if d.To != temporal.DayEnd().AddDate(0, 0, 1) { // +1 because the base date is included
+		t.Error("To date is not today's end date")
+	}
+}
+
+// Tests for DateRange.LastWeek
+func TestDateRangeLastWeek(t *testing.T) {
+	d := temporal.DateRange().LastWeek()
+	e := temporal.LastWeek()
+	for e.Weekday() != time.Sunday {
+		e = e.AddDate(0, 0, -1)
+	}
+
+	if d.From != e {
+		t.Error("From date is not last week's start date")
+	}
+	if d.To != e.AddDate(0, 0, 6) {
+		t.Error("To date is not last week's end date")
+	}
+}
+
+// Tests for DateRange.ThisWeek
+func TestDateRangeThisWeek(t *testing.T) {
+	d := temporal.DateRange().ThisWeek()
+	e := temporal.DayStart()
+	for e.Weekday() != time.Sunday {
+		e = e.AddDate(0, 0, -1)
+	}
+	if d.From != e {
+		t.Error("From date is not this week's start date")
+	}
+	if d.To != e.AddDate(0, 0, 6) {
+		t.Error("To date is not this week's end date")
+	}
+}
+
+// Tests for DateRange.NextWeek
+func TestDateRangeNextWeek(t *testing.T) {
+	d := temporal.DateRange().NextWeek()
+	e := temporal.NextWeek()
+	for e.Weekday() != time.Sunday {
+		e = e.AddDate(0, 0, -1)
+	}
+	if d.From != e {
+		t.Error("From date is not this week's start date")
+	}
+	if d.To != e.AddDate(0, 0, 6) {
+		t.Error("To date is not this week's end date")
 	}
 }
