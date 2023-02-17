979a01a5e9dc1e0767fbb5bc3dc981e4d1db97e9
Kabir Maniar <kabir.maniar@maniartech.com>
2023-02-17 17:38:03 +0530

    Restructured format function

--- format.go
+++ format.go
@@ -1,31 +1,42 @@
 package temporal
 
 import (
-	"fmt"
 	"time"
 )
 
 // Format converts a simple format string to a time.Time format string. It
 // accepts a simple format string such as "yyyy-mm-dd" and returns a converted
 // format string such as "2006-01-02".
-func Format(dt any, format string) (string, error) {
+func Format(dt time.Time, format string) (string, error) {
 	convertedFormat := ConvertFormat(format)
 
-	switch dtType := dt.(type) {
-	case time.Time:
-		return dtType.Format(convertedFormat), nil
-	case string:
-		// Convert string to time.Time using RFC3339 format
-		t, err := time.Parse(time.RFC3339, dtType)
-		if err != nil {
-			return "", err
-		}
-		return t.Format(convertedFormat), nil
-	case int64:
-		return time.Unix(dtType, 0).Format(convertedFormat), nil
-	case uint64:
-		return time.Unix(int64(dtType), 0).Format(convertedFormat), nil
-	}
+	return dt.Format(convertedFormat), nil
+}
 
-	return "", newEror(errInvalidType, errInvalidTypeMsg, fmt.Sprintf("%T", dt))
+func FormatStr(dt string, format string, inputFormat ...string) (string, error) {
+	convertedFormat := ConvertFormat(format)
+	ip := "yyyy-mm-ddThh:mm:ssZ"
+	if len(inputFormat) > 0 {
+		ip = inputFormat[0]
+	}
+	inputFormatFormatted := ConvertFormat(ip)
+
+	// Convert string to time.Time using RFC3339 format
+	t, err := time.Parse(inputFormatFormatted, dt)
+	if err != nil {
+		return "", err
+	}
+	return t.Format(convertedFormat), nil
+}
+
+func FormatInt(dt int64, format string) (string, error) {
+	convertedFormat := ConvertFormat(format)
+
+	return time.Unix(dt, 0).Format(convertedFormat), nil
+}
+
+func FormatUint(dt uint64, format string) (string, error) {
+	convertedFormat := ConvertFormat(format)
+
+	return time.Unix(int64(dt), 0).Format(convertedFormat), nil
 }
