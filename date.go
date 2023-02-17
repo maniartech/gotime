1e491451c1bd0a6f0e5f7104f7b2e5aba9e46e76
Kabir Maniar <kabir.maniar@maniartech.com>
2023-02-17 17:37:34 +0530

    Added Date function

--- date.go
+++ date.go
@@ -4,6 +4,120 @@ import (
 	"time"
 )
 
+// Create a struct called Date
+type date struct {
+	Start time.Time
+	End   time.Time
+}
+
+func Date(dt ...time.Time) date {
+	var t time.Time
+	if len(dt) > 0 {
+		t = dt[0]
+	} else {
+		t = time.Now()
+	}
+	return date{Start: DayStart(t), End: DayEnd(t)}
+}
+
+// Date().Monday(weeks) returns the date of the current week's Monday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Monday
+func (d date) Monday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+1)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Tuesday(weeks) returns the date of the current week's Tuesday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Tuesday
+func (d date) Tuesday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+2)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Wednesday(weeks) returns the date of the current week's Wednesday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Wednesday
+func (d date) Wednesday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+3)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Thursday(weeks) returns the date of the current week's Thursday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Thursday
+func (d date) Thursday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+4)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Friday(weeks) returns the date of the current week's Friday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Friday
+func (d date) Friday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+5)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Saturday(weeks) returns the date of the current week's Saturday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Saturday
+func (d date) Saturday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7+6)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
+// Date().Sunday(weeks) returns the date of the current week's Sunday
+// weeks is the number of weeks to add to the current week
+// If the value is negative, it will return the previous week's Sunday
+func (d date) Sunday(weeks ...int) date {
+	w := 0
+	if len(weeks) > 0 {
+		w = weeks[0]
+	}
+	d.Start = DayStart().AddDate(0, 0, -int(DayStart().Weekday())+w*7)
+	d.End = DayEnd(d.Start)
+
+	return d
+}
+
 // Return today's date at 00:00:00
 func DayStart(dt ...time.Time) time.Time {
 	var t time.Time
