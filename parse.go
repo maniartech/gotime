f891ad133f1e69dbe395f45f00b03a6dd015b45f
Aamir Maniar <aamironline@gmail.com>
2023-04-09 19:57:33 +0530

    Conversion Algorithm WIP

--- parse.go
+++ parse.go
@@ -1,13 +1,17 @@
 package temporal
 
 import (
+	"errors"
 	"time"
 )
 
 // Parse parses a date string and returns the time value it represents.
 // It accepts a date string and a simple format string such as "yyyy-mm-dd".
-func Parse(dt string, format string) (*time.Time, error) {
-	convertedFormat := ConvertFormat(format)
-	parsedTime, err := time.Parse(convertedFormat, dt)
-	return &parsedTime, err
+func Parse(layout, value string) (time.Time, error) {
+	convertedFormat := ConvertFormat(layout)
+	if len(convertedFormat) > 1 {
+		return time.Time{}, errors.New("Ordinals are not supported")
+	}
+	parsedTime, err := time.Parse(convertedFormat[0], value)
+	return parsedTime, err
 }
--- parse_test.go
+++ parse_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestParse(t *testing.T) {
 	// Test case for parsing the date 24-01-1984
-	format, _ := temporal.Parse("24-01-1984", "dd-mm-yyyy")
+	format, _ := temporal.Parse("dd-mm-yyyy", "24-01-1984")
 	correctTime := time.Date(1984, 1, 24, 0, 0, 0, 0, time.UTC)
 	if !format.Equal(correctTime) {
 		t.Errorf("Expected %v, got, %v", correctTime, format)
