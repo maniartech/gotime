6f691489d2fba3ea23bb2fc7fa3720911b9c9187
Aamir Maniar <aamironline@gmail.com>
2024-01-21 18:13:03 +0530

    Add support for parsing date with time zone information

--- internal/idf/parse.go
+++ internal/idf/parse.go
@@ -6,13 +6,18 @@ import (
 
 // Parse parses a date string and returns the time value it represents.
 // It accepts a date string and a simple format string such as "yyyy-mm-dd".
-func Parse(layout, value string) (time.Time, error) {
+func Parse(layout, value string, loc ...*time.Location) (time.Time, error) {
 	convertedFormat, err := convertLayout(layout, true)
 	if err != nil {
 		return time.Time{}, err
 	}
 
 	if str, ok := convertedFormat.(string); ok {
+
+		if len(loc) > 0 {
+			return time.ParseInLocation(str, value, loc[0])
+		}
+
 		return time.Parse(str, value)
 	}
 
