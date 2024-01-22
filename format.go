fe63769f1e1dec0080e36cd0da62a668f78de6ca
Kabir Maniar <kabir.maniar@maniartech.com>
2024-01-22 19:27:29 +0530

    Update dependencies in go.mod and go.sum files Added Tests for Parse Functions

--- format.go
+++ format.go
@@ -56,18 +56,3 @@ func FormatTimestamp(timestamp int64, layout string) string {
 func Convert(value, fromLayout, toLayout string) (string, error) {
 	return idf.Convert(value, fromLayout, toLayout)
 }
-
-// Parse is a utility function that takes a date-time string and a format string
-// as input, then parses the date-time string according to the format. The
-// function returns a time.Time value and an error if the parsing fails.
-//
-// Example usage:
-//
-//	parsedDate, err := Parse("yyyy-mm-dd", "2022-12-31")
-//	if err != nil {
-//	    panic(err)
-//	}
-//	fmt.Println(parsedDate) // 2022-12-31 00:00:00 +0000 UTC
-func Parse(layout, value string) (time.Time, error) {
-	return idf.Parse(layout, value)
-}
