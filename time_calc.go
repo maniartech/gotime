02fe5d790ad52d944c23371f8be1be9ddd0c7cf5
Kabir Maniar <kabir.maniar@maniartech.com>
2024-01-26 19:59:33 +0530

    Add DateValue and WorkDay functions to calculate date values and work days

--- time_calc.go
+++ time_calc.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+func DateValue(date time.Time) float64 {
+	var val time.Time = time.Date(1900, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
+
+	val = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.UTC)
+
+	// Converting the time.Time form to a serial number starting from 1/1/1900
+	diff := val.Sub(time.Date(1900, 1, 1, 0, 0, 0, 0, date.Location())).Hours()
+	return float64(diff/24) + 2
+}
+
 // Diff returns the difference between the given time.Time and the current time.Time in the given unit
 func Diff(t1, t2 time.Time, unit time.Duration, rounded ...bool) float64 {
 	isRounded := false
@@ -70,3 +80,38 @@ func TruncateTime(date time.Time) time.Time {
 	)
 }
 
+func WorkDay(startDate time.Time, days float64, workingDays [7]bool, holidays ...time.Time) time.Time {
+	finalDateSerial := DateValue(startDate)
+	weekDay := startDate.Weekday()
+
+	// truncating the days to get the integer part
+	days = float64(int(days))
+
+	holidaysSerial := make([]float64, len(holidays))
+	for i, holiday := range holidays {
+		holidaysSerial[i] = DateValue(holiday)
+	}
+
+	for days > 0 {
+		finalDateSerial++
+		weekDay = (weekDay + 1) % 7
+		if !workingDays[weekDay] {
+			continue
+		}
+		found := false
+		for _, holiday := range holidaysSerial {
+			if finalDateSerial == holiday {
+				holidaysSerial = holidaysSerial[1:]
+				found = true
+			}
+			if found {
+				break
+			}
+		}
+		if !found {
+			days--
+		}
+	}
+
+	return time.Date(1900, time.Month(1), 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(finalDateSerial-2) * 24 * time.Hour)
+}
