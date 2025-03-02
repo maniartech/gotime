1db23ecc57c5222ade252c28bfeb1fdb33c841a9
Aamir Maniar <aamironline@gmail.com>
2025-03-02 05:42:58 +0530

    Rename ParseAndFormat function to Convert and update example usage

--- convert.go
+++ convert.go
@@ -2,7 +2,7 @@ package gotime
 
 import "github.com/maniartech/gotime/internal/idfs"
 
-// ParseAndFormat is a utility function that takes a date-time string, a source
+// Convert is a utility function that takes a date-time string, a source
 // format string (from), and a target format string (to). The function parses
 // the input date-time string according to the source format, and then formats
 // the parsed date-time value using the target format. The function returns the
@@ -10,9 +10,10 @@ import "github.com/maniartech/gotime/internal/idfs"
 //
 // Example usage:
 //
-//	formattedDate, err := ParseAndFormat("2022-12-31", "yyyy-mm-dd", "dt mmmm, yyyy")
+//	formattedDate, err := Convert("2022-12-31", "yyyy-mm-dd", "dt mmmm, yyyy")
 //	if err != nil {
-//		panic(err)
+//		log.Printf("Failed to convert date: %v", err)
+//		return
 //	}
 //	fmt.Println(formattedDate) // 31st December, 2022
 func Convert(value, fromLayout, toLayout string) (string, error) {
